816624c52b08bed9b3b1a85c29ead5520bd0cc2e
Nikola marincic <nikola.marincic@gmail.com>
2016-10-10 18:07:31 +0200

    Added sublime project

--- wikiparser/__main__.py
+++ wikiparser/__main__.py
@@ -5,14 +5,13 @@ import mwparserfromhell
 
 def main():
         
+    data_base = db.connect_to_database("../database.db")
+    words = db.get_all_words(data_base, "words")
 
-        
-    #data_base = db.connect_to_database("database.db")
-    #words = db.get_all_words(data_base, "words")
-    #all_records = db.get_all_records(data_base, 'words')
+    all_records = db.get_all_records(data_base, 'words')
     
-    #db.print_records(data_base, "words", 10000)
-    #print (words)
+    db.print_records(data_base, "words", 10000)
+    print (words)
     
     word = []
     page = pp.parse_wiki_page("über")
@@ -20,7 +19,7 @@ def main():
         word.append(page['title'])
         word.append(page['pageid'])
         word.append(page['wikitext'])
-        #db.insert_word_data(data_base, "words", word)
+        db.insert_word_data(data_base, "words", word)
     else:
         print ('error')
 
@@ -29,7 +28,7 @@ def main():
     sections = wikicode.get_sections()
     for s in sections:
         print (s)
-    # db.close_connection(data_base)
+    db.close_connection(data_base)
     
 if __name__ == "__main__":
     main()
